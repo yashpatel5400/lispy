8858d7a7ac15c789a17892a6a2fc31873a59dd0f
yashpatel5400 <ypatel5400@gmail.com>
2021-01-24 19:48:18 -0500

    Initialize to status at chapter 7

--- Proj/Proj/prompt.c
+++ Proj/Proj/prompt.c
@@ -28,6 +28,46 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+int number_of_nodes(mpc_ast_t* t) {
+    if (t->children_num == 0) { return 1; }
+    if (t->children_num >= 0) { 
+        int total = 1;
+
+        for (int i = 0; i < t->children_num; i++) {
+            total += number_of_nodes(t->children[i]);
+        }
+        return total; 
+    }
+
+    return 0;
+}
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     mpc_parser_t* Number   = mpc_new("number");
@@ -55,13 +95,13 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
         free(input);
 
     }
