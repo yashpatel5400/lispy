38869f47a3e70dcbadd8e1fba8b6b1cff1b1a8ae
yashpatel5400 <ypatel5400@gmail.com>
2021-01-24 20:04:59 -0500

    Initialize to status at chapter 7

--- Proj/Proj/prompt.c
+++ Proj/Proj/prompt.c
@@ -7,7 +7,56 @@
 #ifdef _WIN32
 #include <string.h>
 
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
 static char buffer[2048];
+
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch (v.type) {
+    case LVAL_NUM: 
+        printf("%li", v.num); 
+        break;
+    case LVAL_ERR:
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division By Zero!");
+        }
+        if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid Operator!");
+        }
+        if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid Number!");
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void lval_println(lval v) {
+    lval_print(v); 
+    putchar('\n');
+}
 
 /* Fake readline function */
 char* readline(char* prompt) {
@@ -42,22 +91,32 @@ int number_of_nodes(mpc_ast_t* t) {
     return 0;
 }
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+lval eval_op(lval x, char* op, lval y) {
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) { 
+        return y.num == 0 
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num); 
+    }
+
+    return lval_err(LERR_BAD_OP);
 }
 
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     char* op = t->children[1]->contents;
 
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
@@ -95,9 +154,10 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
+
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
