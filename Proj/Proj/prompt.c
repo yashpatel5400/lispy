899f2558bd668b9ed40e004dda8adb20342e05b1
yashpatel5400 <ypatel5400@gmail.com>
2021-01-31 16:33:55 -0500

    Finished Q expressions

--- Proj/Proj/prompt.c
+++ Proj/Proj/prompt.c
@@ -127,6 +127,17 @@ lval* lval_take(lval* v, int i) {
     return x;
 }
 
+lval* lval_join(lval* x, lval* y) {
+    while (y->count) {
+        x = lval_add(x, lval_pop(y, 0));
+    }
+
+    lval_del(y);
+    return x;
+}
+
+lval* lval_eval(lval* v);
+
 void lval_print(lval* v);
 void lval_expr_print(lval* v, char open, char close) {
     putchar(open);
@@ -155,48 +166,56 @@ void lval_print(lval* v) {
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
 lval* builtin_head(lval* a) {
-    if (a->count == 1) {
-        lval_del(a);
-        return lval_err("Function has incorrect number of arguments");
-    }
-
-    if (a->cell[0]->type != LVAL_QEXPR) {
-        lval_del(a);
-        return lval_err("Function 'head' passed incorrect types!");
-    }
-
-    if (a->cell[0]->count == 0) {
-        lval_del(a);
-        return lval_err("Head of an empty list!");
-    }
+    LASSERT(a, a->count == 1, "Function has incorrect number of arguments");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect types!");
+    LASSERT(a, a->cell[0]->count != 0, "Head of an empty list!");
 
     lval* v = lval_take(a, 0);
-
     while (v->count > 1) { lval_del(lval_pop(v, 1)); }
     return v;
 }
 
 lval* builtin_tail(lval* a) {
-    if (a->count == 1) {
-        lval_del(a);
-        return lval_err("Function has incorrect number of arguments");
-    }
-
-    if (a->cell[0]->type != LVAL_QEXPR) {
-        lval_del(a);
-        return lval_err("Function 'tail' passed incorrect types!");
-    }
-
-    if (a->cell[0]->count == 0) {
-        lval_del(a);
-        return lval_err("Tail of an empty list!");
-    }
+    LASSERT(a, a->count == 1, "Function has incorrect number of arguments");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect types!");
+    LASSERT(a, a->cell[0]->count != 0, "Head of an empty list!");
 
     lval* v = lval_take(a, 0);
-
     lval_del(lval_pop(v, 0));
     return v;
+}
+
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count == 1, "Function eval passed too many arguments");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function eval passed too many arguments");
+
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+lval* builtin_join(lval* a) {
+    for (int i = 0; i < a->count; i++) {
+        LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+            "Function 'join' passed incorrect type.");
+    }
+
+    lval* x = lval_pop(a, 0);
+    while (a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(a);
+    return x;
 }
 
 lval* builtin_op(lval* a, char* op) {
@@ -245,6 +264,17 @@ lval* builtin_op(lval* a, char* op) {
     return x;
 }
 
+lval* builtin(lval* a, char* func) {
+    if (strcmp("list", func) == 0) { return builtin_list(a); }
+    if (strcmp("head", func) == 0) { return builtin_head(a); }
+    if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+    if (strcmp("join", func) == 0) { return builtin_join(a); }
+    if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+    if (strstr("+-/*", func)) { return builtin_op(a, func); }
+    lval_del(a);
+    return lval_err("Unknown Function!");
+}
+
 lval* lval_read_num(mpc_ast_t* t) {
     errno = 0;
     long x = strtol(t->contents, NULL, 10);
@@ -277,7 +307,6 @@ lval* lval_read(mpc_ast_t* t) {
     return x;
 }
 
-lval* lval_eval(lval* v);
 lval* lval_eval_sexpr(lval* v) {
     for (int i = 0; i < v->count; i++) {
         v->cell[i] = lval_eval(v->cell[i]);
@@ -298,7 +327,7 @@ lval* lval_eval_sexpr(lval* v) {
         return lval_err("S-Expression does not start w/ symbol!");
     }
 
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
 }
